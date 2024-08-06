aa1bb92ee5a3072bda4648ef74ce6a5ec7fce725
Jessica De Silva <74026509+jessicadesilva@users.noreply.github.com>
2024-08-06 15:16:44 -0700

    Create task_1.py

---
+++ homework/task_1.py
@@ -0,0 +1,17 @@
+# Task: Represent common single-variable mathematical functions in Python.
+
+# 1. Define a function called linear_function that represents the linear function f(x) = 2x+3.
+
+
+
+# 2. Define a function called constant_function that represents the constant function g(x) = 4.
+
+
+
+# 3. Define a function called exponential_function that represents the exponential function h(x) = 2^x.
+
+
+
+# 4. Use a built-in command to define a function called absolute_value that reperesents the absolute value function k(x)=|x|.
+
+
