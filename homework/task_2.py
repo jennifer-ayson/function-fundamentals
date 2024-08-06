163bd0bcc3d610e4d0e474bb1f101c6bdf01970d
Jessica De Silva <74026509+jessicadesilva@users.noreply.github.com>
2024-08-06 15:27:22 -0700

    Create task_2.py

---
+++ homework/task_2.py
@@ -0,0 +1,18 @@
+# Task: Represent common multi-variable mathematical functions in Python.
+
+# Task 1: Define a function called average_value that takes in two numerical inputs and outputs their average.
+
+
+
+# Task 2: Define a function called max_of_three that takes in three numerical inputs and outputs the maximum value.
+
+
+
+# Task 3: Define a function called min_of_three that takes in three numerical inputs and outputs the minimum value.
+
+
+
+# Task 4: Define a function called midrange that takes in three numerical inputs and outputs the average of the maximum
+# and minimum values. This function should call the functions created in tasks 1 - 3.
+
+
