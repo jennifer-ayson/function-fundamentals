d6cfad2a27f20c173998e5af1831d3baad72ed6e
Jessica De Silva <74026509+jessicadesilva@users.noreply.github.com>
2024-08-06 15:37:28 -0700

    Create task_3.py

---
+++ homework/task_3.py
@@ -0,0 +1,24 @@
+# Task: Represent functions with non-numerical parameters.
+
+# Task 1: Define a function called first_element that takes in a list and outputs its first element.
+
+
+
+# Task 2: Define a function called last_element that takes in a list and outputs its last element.
+
+
+
+# Task 3: Define a function called first_last_sum that takes in a list and outputs the sum of
+# the first and last element. The function should call the functions in tasks 1-2.
+
+
+
+# Task 4: Define a function called distance_from_origin that takes in a tuple representing a point
+# in the xy-coordinate plane and outputs its distance from the origin.
+
+
+
+# Task 5: Define a function called distance_between that takes in two tuples representing points
+# in the xy-coordinate plane and outputs their distance from each other.
+
+
